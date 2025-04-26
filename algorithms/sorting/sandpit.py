27feeb4ab3c87d90b7a06e9b09b6a3ea8554baab
Dana Goosen <klymentovska@gmail.com>
2025-04-26 09:02:51 +0200

    Update the sandpit

--- algorithms/sorting/sandpit.py
+++ algorithms/sorting/sandpit.py
@@ -1,22 +1,22 @@
 from random import randint
 import time
 
-from bubble.bubble import bubble_sort as bubble
-from selection.selection import selection_sort as selection
-from insertion.insertion import insertion_sort as insertion
-from shell.shell import shell_sort as shell
+from bubble.bubble import bubble_sort
+from selection.selection import selection_sort
+from insertion.insertion import insertion_sort
+from shell.shell import shell_sort
 
 def get_random_numbers(n):
     return [randint(0, n) for i in range(n)]
 
 sorts = {
-    'Bubble sort': bubble,
-    'Selection sort': selection,
-    'Insertion sort': insertion,
-    'Shell sort': shell,
+    'Bubble sort': bubble_sort,
+    'Selection sort': selection_sort,
+    'Insertion sort': insertion_sort,
+    'Shell sort (Sedgewick)': shell_sort,
     }
 
-for i in [500, 1000, 5000, 10000]:
+for i in [500, 1000, 5000, 10000, 15000]:
     print(f'\nTESTING {i} ELEMENTS')
     random_array = get_random_numbers(i)
     
