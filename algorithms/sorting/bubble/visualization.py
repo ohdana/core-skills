eced6a5df4fb53bd72290cbaf4f26b76eb3102aa
Dana Goosen <klymentovska@gmail.com>
2025-04-27 18:20:48 +0200

    Fix typos

--- algorithms/sorting/bubble/visualization.py
+++ algorithms/sorting/bubble/visualization.py
@@ -11,7 +11,7 @@ class Main:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Insertion Sort')
+        pygame.display.set_caption('Bubble Sort')
         self.clock = pygame.time.Clock()
         self.generate_numbers()
         self.is_running = True
